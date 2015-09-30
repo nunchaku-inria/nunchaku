e6fa93b59f617dd28926a2fc502deef2baba533e
Simon Cruanes <simon.cruanes.2007@m4x.org>
2015-09-30 16:51:34 +0200

    add some utils, including debugging

--- src/core/nunUtils.ml
+++ src/core/nunUtils.ml
@@ -1,4 +1,125 @@
 
 (* This file is free software, part of containers. See file "license" for more details. *)
 
+type 'a sequence = ('a -> unit) -> unit
+
+module Time = struct
+  (** Time elapsed since initialization of the program, and time of start *)
+  let total, start =
+    let start = Unix.gettimeofday () in
+    (function () ->
+      let stop = Unix.gettimeofday () in
+      stop -. start),
+    (function () -> start)
+end
+
+(** {2 Debug} *)
+
+(** Debug section *)
+module Section = struct
+  let null_level = -1 (* absence of level *)
+  type t = {
+    descr : descr;
+    mutable full_name : string;
+    mutable level : int;
+  }
+  and descr =
+    | Root
+    | Sub of string * t * t list  (* name, parent, inheriting *)
+
+  let root={descr=Root; full_name=""; level=0; }
+
+  (* computes full name of section *)
+  let compute_full_name s =
+    let buf = Buffer.create 15 in
+    let rec add s = match s.descr with
+      | Root -> true
+      | Sub (name, parent, _) ->
+          let parent_is_root = add parent in
+          if not parent_is_root then Buffer.add_char buf '.';
+          Buffer.add_string buf name;
+          false
+    in
+    ignore (add s);
+    Buffer.contents buf
+
+  let full_name s = s.full_name
+
+  (* full name -> section *)
+  let section_table = Hashtbl.create 15
+
+  let set_debug s i = assert (i>=0); s.level <- i
+  let clear_debug s = s.level <- null_level
+  let get_debug s =
+    if s.level=null_level then None else Some s.level
+
+  let make ?(parent=root) ?(inheriting=[]) name =
+    if name="" then invalid_arg "Section.make: empty name";
+    let sec = {
+      descr=Sub(name, parent, inheriting);
+      full_name="";
+      level=null_level;
+    } in
+    let name' = compute_full_name sec in
+    try
+      Hashtbl.find section_table name'
+    with Not_found ->
+      (* new section! register it, add an option to set its level *)
+      sec.full_name <- name';
+      Hashtbl.add section_table name' sec;
+      sec
+
+  let iter yield =
+    yield ("", root);
+    Hashtbl.iter (fun name sec -> yield (name,sec)) section_table
+
+  (* recursive lookup, with inheritance from parent *)
+  let rec cur_level_rec s =
+    if s.level = null_level
+    then match s.descr with
+      | Root -> 0
+      | Sub (_, parent, []) -> cur_level_rec parent
+      | Sub (_, parent, [i]) -> max (cur_level_rec parent) (cur_level_rec i)
+      | Sub (_, parent, inheriting) ->
+          List.fold_left
+            (fun m i -> max m (cur_level_rec i))
+            (cur_level_rec parent) inheriting
+    else s.level
+
+  (* inlinable function *)
+  let cur_level s =
+    if s.level = null_level
+      then cur_level_rec s
+      else s.level
+end
+
+let set_debug = Section.set_debug Section.root
+let get_debug () = Section.root.Section.level
+
+let debug_fmt_ = Format.err_formatter
+
+let debugf ?(section=Section.root) l msg =
+  if l <= Section.cur_level section
+    then (
+      let now = Time.total () in
+      if section == Section.root
+        then Format.fprintf debug_fmt_ "@[<hov>%% [%.3f] " now
+        else Format.fprintf debug_fmt_ "@[<hov>%% [%.3f %s] "
+          now section.Section.full_name;
+        Format.kfprintf
+          (fun fmt -> Format.fprintf fmt "@]@.")
+          debug_fmt_ msg
+    ) else Format.ifprintf debug_fmt_ msg
+
+(** {2 Misc} *)
+
+exception NotImplemented of string
+
+let () = Printexc.register_printer
+  (function
+    | NotImplemented s -> Some ("feature " ^ s ^ " is not implemented")
+    | _ -> None
+  )
+
+let not_implemented feat = raise (NotImplemented feat)
 
--- src/core/nunUtils.mli
+++ src/core/nunUtils.mli
@@ -2,3 +2,66 @@
 (* This file is free software, part of containers. See file "license" for more details. *)
 
 (** {1 Utils} *)
+
+type 'a sequence = ('a -> unit) -> unit
+
+(** {2 Time} *)
+
+module Time : sig
+  val total : unit -> float
+  (** time elapsed since start of program *)
+
+  val start : unit -> float
+  (** time at which the program started *)
+end
+
+(** {2 Debugging} *)
+
+(** Debug section *)
+module Section : sig
+  type t
+
+  val full_name : t -> string  (** Full path to the section *)
+
+  val set_debug : t -> int -> unit
+  (** Debug level for section (and its descendants) *)
+
+  val clear_debug : t -> unit
+  (** Clear debug level (will be same as parent's) *)
+
+  val get_debug : t -> int option
+  (** Specific level of this section, if any *)
+
+  val cur_level : t -> int
+  (** Current debug level, with parent inheritance *)
+
+  val iter : (string * t) sequence
+  (** all registered sections *)
+
+  val root : t
+  (** Default section, with no parent *)
+
+  val make : ?parent:t -> ?inheriting:t list -> string -> t
+  (** [make ?parent ?inheriting name] makes a new section with the given name.
+      It has a parent (default [root]), used to give it a name. It can
+      also have a list of sections it inherits from.
+      Unless specificed explicitely otherwise (using
+      {!set_debug}, the level of the section will be the max level of its
+      parent and its inherited sections. *)
+end
+
+val set_debug : int -> unit     (** Set debug level of [Section.root] *)
+val get_debug : unit -> int     (** Current debug level for [Section.root] *)
+
+val debugf : ?section:Section.t -> int ->
+            ('a, Format.formatter, unit, unit) format4 -> 'a
+(** Print a debug message, with the given section and verbosity level.
+    The message might be dropped if its level is too high. *)
+
+(** {2 Misc} *)
+
+exception NotImplemented of string
+
+val not_implemented: string -> 'a
+(** Raise an exception saying the given feature is not implemented *)
+
