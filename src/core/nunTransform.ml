6bc9bbe28e2d9a276f884a0df8e952ee87113f76
Simon Cruanes <simon.cruanes.2007@m4x.org>
2015-10-05 18:27:13 +0200

    more general type for the pipeline

--- src/core/nunTransform.ml
+++ src/core/nunTransform.ml
@@ -11,17 +11,18 @@ type 'a lazy_list = unit -> [`Nil | `Cons of 'a * 'a lazy_list]
 (** Transformation of ['a] to ['b]. The transformation make choices by
     lazily returning several values. It is also allowed to store data
     in a internal "state" type, to be able to transform back later. *)
-type ('a, 'b) t = Ex : ('a, 'b, 'st) inner -> ('a, 'b) t
-
 (** Transformation with explicit hidden state *)
-and ('a, 'b, 'st) inner = {
+type ('a, 'b, 'c, 'd) t = Ex : ('a, 'b, 'c, 'd, 'st) inner -> ('a, 'b, 'c, 'd) t
+
+and ('a, 'b, 'c, 'd, 'st) inner = {
   name : string; (** informal name for the transformation *)
   encode : 'a -> ('b * 'st) lazy_list;
-  decode : 'st -> 'b -> 'a;
+  decode : 'st -> 'c -> 'd;
   print_state : (Format.formatter -> 'st -> unit) option;  (** Debugging *)
 }
 
-type ('a, 'b) transformation = ('a, 'b) t
+type ('a, 'b, 'c, 'd) transformation = ('a, 'b, 'c, 'd) t
+(** Alias to {!t} *)
 (** Alias to {!t} *)
 
 (** {2 Pipeline of Transformations}
@@ -29,16 +30,16 @@ type ('a, 'b) transformation = ('a, 'b) t
     Allows chaining the transformations in a type-safe way *)
 
 module Pipe = struct
-  type ('a, 'b) t =
-    | Id : ('a, 'a) t  (** no transformation *)
-    | Comp : ('a, 'b) transformation * ('b, 'c) t -> ('a, 'c) t
+  type ('a, 'b, 'c, 'd) t =
+    | Id : ('a, 'a, 'c, 'c) t  (** no transformation *)
+    | Comp : ('a, 'b, 'd, 'e) transformation * ('b, 'b2, 'c, 'd) t -> ('a, 'b2, 'c, 'e) t
 
   let id = Id
   let compose a p = Comp (a, p)
 end
 
 let rec run
-  : type a b. pipe:(a,b) Pipe.t -> a -> (b * (b -> a)) lazy_list
+  : type a b c d. pipe:(a,b,c,d) Pipe.t -> a -> (b * (c -> d)) lazy_list
   = fun ~pipe x -> match pipe with
   | Pipe.Id -> CCKList.return (x, fun x->x)
   | Pipe.Comp (Ex trans, pipe') ->
--- src/core/nunTransform.mli
+++ src/core/nunTransform.mli
@@ -8,20 +8,22 @@ type 'a lazy_list = unit -> [`Nil | `Cons of 'a * 'a lazy_list]
 
 (** {2 Single Transformation} *)
 
-(** Transformation of ['a] to ['b]. The transformation make choices by
+(** Transformation of ['a] to ['b], with reverse transformation from ['c]
+    to ['d]. The transformation make choices by
     lazily returning several values. It is also allowed to store data
-    in a internal "state" type, to be able to transform back later. *)
-type ('a, 'b) t = Ex : ('a, 'b, 'st) inner -> ('a, 'b) t
+    in a internal "state" type, to be able to perform the reverse
+    transformation. *)
+type ('a, 'b, 'c, 'd) t = Ex : ('a, 'b, 'c, 'd, 'st) inner -> ('a, 'b, 'c, 'd) t
 
 (** Transformation with explicit hidden state *)
-and ('a, 'b, 'st) inner = {
+and ('a, 'b, 'c, 'd, 'st) inner = {
   name : string; (** informal name for the transformation *)
   encode : 'a -> ('b * 'st) lazy_list;
-  decode : 'st -> 'b -> 'a;
+  decode : 'st -> 'c -> 'd;
   print_state : (Format.formatter -> 'st -> unit) option;  (** Debugging *)
 }
 
-type ('a, 'b) transformation = ('a, 'b) t
+type ('a, 'b, 'c, 'd) transformation = ('a, 'b, 'c, 'd) t
 (** Alias to {!t} *)
 
 
@@ -30,17 +32,22 @@ type ('a, 'b) transformation = ('a, 'b) t
     Allows chaining the transformations in a type-safe way *)
 
 module Pipe : sig
-  (** Composite transformation from ['a] to ['b], yielding results ['res] *)
-  type ('a, 'b) t =
-    | Id : ('a, 'a) t  (** no transformation *)
-    | Comp : ('a, 'b) transformation * ('b, 'c) t -> ('a, 'c) t
+  (** Composite transformation from ['a] to ['b], with a reverse transformation
+      from ['c] to ['d] *)
+  type ('a, 'b, 'c, 'd) t =
+    | Id : ('a, 'a, 'c, 'c) t  (** no transformation *)
+    | Comp : ('a, 'b, 'd, 'e) transformation * ('b, 'b2, 'c, 'd) t -> ('a, 'b2, 'c, 'e) t
 
-  val id : ('a, 'a) t
+  val id : ('a, 'a, 'c, 'c) t
 
-  val compose : ('a, 'b) transformation -> ('b, 'c) t -> ('a, 'c) t
+  val compose :
+    ('a, 'b, 'd1, 'e) transformation ->
+    ('b, 'b2, 'c, 'd1) t -> ('a, 'b2, 'c, 'e) t
 end
 
-val run : pipe:('a, 'b) Pipe.t -> 'a -> ('b * ('b -> 'a)) lazy_list
+val run : pipe:('a, 'b, 'c, 'd) Pipe.t ->
+          'a ->
+          ('b * ('c -> 'd)) lazy_list
 (** [run ~pipe x] runs [x] through the pipe [pipe], in a lazy way,
     and yields values of type ['b] along with a conversion function back *)
 
