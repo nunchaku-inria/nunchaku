5486e703e73c136eb91d86a6005080164c3e3e39
Simon Cruanes <simon.cruanes.2007@m4x.org>
2015-11-09 17:54:37 +0100

    wip: flattening of equations (seems to mostly work)

--- src/core/transformations/nunFlattenEquation.ml
+++ src/core/transformations/nunFlattenEquation.ml
@@ -25,6 +25,7 @@ let () = Printexc.register_printer
 
 let error_ msg = raise (Error msg)
 let errorf_ fmt = NunUtils.exn_ksprintf fmt ~f:(fun msg -> error_ msg)
+let section = NunUtils.Section.make "flatten_eqn"
 
 module Make(T : NunTerm_ho.S) = struct
   module U = NunTerm_ho.Util(T)
@@ -39,23 +40,36 @@ module Make(T : NunTerm_ho.S) = struct
     | EqTerm of term * term
     | Test of term * ID.t (* [t,cons] --> is_cons t *)
 
+  let fpf = Format.fprintf
+  let print_term = NunTerm_ho.print ~repr:T.repr
+
+  let pp_constraint out = function
+    | EqTerm (t1,t2) -> fpf out "@[<2>%a =@ %a@]" print_term t1 print_term t2
+    | Test (t, id) -> fpf out "@[is-%a %a@]" ID.print_name id print_term t
+
   (* a local context for flattening patterns and equations *)
   type ctx = {
     env : (term, term, [`Linear]) Env.t;
-    vars: var list; (* already introduced variables *)
+    blocked_vars: var list; (* already introduced variables *)
     c_set: constraint_ list; (* constraints *)
     subst: (term, term) Subst.t (* substitution to apply to RHS *)
   }
 
-  let print_term = NunTerm_ho.print ~repr:T.repr
+  let empty_ctx ~env = { blocked_vars=[]; c_set=[]; subst=Subst.empty; env; }
 
-  let empty_ctx ~env = { vars=[]; c_set=[]; subst=Subst.empty; env; }
+  let add_constr_ ~ctx c =
+    NunUtils.debugf ~section 4 "add constraint %a" (fun k-> k pp_constraint c);
+    {ctx with c_set=c::ctx.c_set; }
 
-  let add_constr_ ~ctx c = {ctx with c_set=c::ctx.c_set; }
-  let add_subst_ ~ctx v t = {ctx with subst=Subst.add ~subst:ctx.subst v t;}
-  let add_var_ ~ctx v = {ctx with vars=v::ctx.vars; }
+  let add_subst_ ~ctx v t =
+    NunUtils.debugf ~section 4 "add binding %a -> `%a`"
+      (fun k-> k Var.print v print_term t);
+    {ctx with subst=Subst.add ~subst:ctx.subst v t;}
 
-  let mk_imply_ a b = U.app_builtin B.T.Imply [a;b]
+  let add_var_ ~ctx v =
+    NunUtils.debugf ~section 4 "block var %a" (fun k-> k Var.print v);
+    {ctx with blocked_vars=v::ctx.blocked_vars; }
+
   let mk_data_select_ a ~id i = U.app_builtin (B.T.DataSelect (id,i)) [a]
   let mk_data_test_ a ~id = U.app_builtin (B.T.DataTest id) [a]
 
@@ -69,51 +83,19 @@ module Make(T : NunTerm_ho.S) = struct
     | TyI.Arrow (a,ty') -> a :: ty_args_ ty'
 
   (* [v] already used in the pattern? *)
-  let already_used_ ~ctx v = List.exists (Var.equal v) ctx.vars
-
-  (* @return (v, ctx') where [ctx'] is a superset of [ctx] and [v] is a
-     variable that does not occur in [ctx.vars]
-     @param ty the type of the argument
-     @param ctx the context, containing the current substitution, etc.
-    *)
-  let rec flatten_pat_
-  : ctx:ctx -> ty:term -> term -> ctx * var
-  = fun ~ctx ~ty t ->
-    match T.repr t with
-    | TI.Var v ->
-        if already_used_ ~ctx v
-        then
-          (* make a new var and ask for it to be equal to [v] *)
-          let v' = Var.fresh_copy v in
-          let ctx = add_constr_ ~ctx (EqTerm (U.var v, U.var v')) in
-          ctx, v'
-        else ctx, v (* use [v] directly as a pattern *)
-    | TI.Const _
-    | TI.App _
-    | TI.Let _
-    | TI.Match _
-    | TI.Bind ((TI.Forall | TI.Exists | TI.Fun),_,_)
-    | TI.AppBuiltin _ ->
-        (* replace [t] with a fresh var [v] and add [v = t] as a guard
-          to the RHS. *)
-        let v = Var.make ~ty ~name:"v" in
-        (* add constraints for [v = t] *)
-        let ctx = mk_eq ~ctx ~to_:t (U.var v) in
-        ctx, v
-    | TI.TyBuiltin _
-    | TI.TyArrow (_,_) -> errorf_ "expected pattern, got %a" print_term t
+  let blocked_var_ ~ctx v = List.exists (Var.equal v) ctx.blocked_vars
 
   (* add enough constraints for making [t], a sub-pattern, equal to
      the term [to_].
      Returns a context updated with the necessary substitutions and constraints *)
-  and mk_eq
+  let rec mk_eq
   : ctx:ctx -> to_:term -> term -> ctx
   = fun ~ctx ~to_ t ->
     let t = SubstUtil.deref ~subst:ctx.subst t in
     match T.repr t with
     | TI.Var v ->
         assert (not (Subst.mem ~subst:ctx.subst v));
-        if already_used_ ~ctx v
+        if blocked_var_ ~ctx v
         then add_constr_ ~ctx (EqTerm (t, to_)) (* variable already bound *)
         else add_subst_ ~ctx v to_ (* TODO: occur check? *)
     | TI.Const id -> mk_eq_constr ~ctx ~to_ id []
@@ -143,10 +125,10 @@ module Make(T : NunTerm_ho.S) = struct
     (* it must be (co)inductive (for now) *)
     match info.Env.def with
     | Env.Cstor (_, _, _, cstor) ->
-        (* we have [c l1...ln], ensure that [to_ = c to1...ton]
-            and that [li = toi] *)
+        (* we have [c l_1...l_n], ensure that [to_ = c to_1...to_n]
+            and that [l_i = to_i] *)
         assert (List.length l = List.length cstor.Stmt.cstor_args);
-        let id = cstor.Stmt.cstor_name in
+        assert (ID.equal id cstor.Stmt.cstor_name);
         let ctx = add_constr_ ~ctx (Test (to_, id)) in
         CCList.Idx.foldi
           (fun ctx i li -> mk_eq ~ctx li ~to_:(mk_data_select_ to_ ~id i))
@@ -154,8 +136,43 @@ module Make(T : NunTerm_ho.S) = struct
     | _ ->
         errorf_ "%a is not a (co)inductive constructor" ID.print_name id
 
+  (* @return (v, ctx') where [ctx'] is a superset of [ctx] and [v] is a
+     variable that does not occur in [ctx.vars]
+     @param ty the type of the argument
+     @param ctx the context, containing the current substitution, etc.
+    *)
+  let flatten_pat_
+  : ctx:ctx -> ty:term -> term -> ctx * var
+  = fun ~ctx ~ty t ->
+    NunUtils.debugf ~section 3 "@[<2>flatten pattern `@[%a@]`@]" (fun k->k print_term t);
+    match T.repr t with
+    | TI.Var v ->
+        if blocked_var_ ~ctx v
+        then
+          (* make a new var and ask for it to be equal to [v] *)
+          let v' = Var.fresh_copy v in
+          let ctx = add_constr_ ~ctx (EqTerm (U.var v, U.var v')) in
+          ctx, v'
+        else ctx, v (* use [v] directly as a pattern *)
+    | TI.Const _
+    | TI.App _
+    | TI.Let _
+    | TI.Match _
+    | TI.Bind ((TI.Forall | TI.Exists | TI.Fun),_,_)
+    | TI.AppBuiltin _ ->
+        (* replace [t] with a fresh var [v] and add [v = t] as a guard
+          to the RHS. *)
+        (* TODO: use [head ty] as a name? *)
+        let v = Var.make ~ty ~name:"v" in
+        (* add constraints for [v = t] *)
+        let ctx = mk_eq ~ctx ~to_:(U.var v) t in
+        ctx, v
+    | TI.TyBuiltin _
+    | TI.TyArrow (_,_) -> errorf_ "expected pattern, got %a" print_term t
+
+
   let flatten_eqn ~defined ~env e =
-    let Stmt.Eqn_nested (_vars,args,rhs) = e in
+    let Stmt.Eqn_nested (_vars,args,rhs,side) = e in
     (* type of the defined term *)
     let ty_head = find_ty_ ~env defined.Stmt.defined_head in
     let ty_args = ty_args_ ty_head in
@@ -165,23 +182,28 @@ module Make(T : NunTerm_ho.S) = struct
     let ctx, vars = NunUtils.fold_map
       (fun ctx (arg,ty_arg) ->
         let ctx, v = flatten_pat_ ~ctx ~ty:ty_arg arg in
-        (* forbid [v] from being used as a pattern in next arguments *)
+        (* forbid [v] from being used as a pattern in next arguments, for
+          that would break linearity *)
         let ctx' = add_var_ ~ctx v in
         ctx', v
       ) (empty_ctx ~env) (List.combine args ty_args)
     in
     let rhs' = SubstUtil.eval ~subst:ctx.subst rhs in
-    (* add constraints to rhs *)
-    let rhs' = List.fold_left
-      (fun rhs constr -> match constr with
+    let side = List.map (SubstUtil.eval ~subst:ctx.subst) side in
+    (* add constraints to [side] *)
+    let side' = List.map
+      (fun constr -> match constr with
         | EqTerm (t1,t2) ->
             (* t1=t2 => rhs *)
-            mk_imply_ (U.eq t1 t2 ) rhs
+            let t1 = SubstUtil.eval ~subst:ctx.subst t1 in
+            let t2 = SubstUtil.eval ~subst:ctx.subst t2 in
+            U.eq t1 t2
         | Test (t, id) ->
-            mk_imply_ (mk_data_test_ ~id t) rhs
-      ) rhs' ctx.c_set
+            let t = SubstUtil.eval ~subst:ctx.subst t in
+            mk_data_test_ ~id t
+      ) ctx.c_set
     in
-    Stmt.Eqn_linear (vars, rhs')
+    Stmt.Eqn_linear (vars, rhs', List.rev_append side' side)
 
   (* translate one single statement *)
   let tr_statement ~env st =
