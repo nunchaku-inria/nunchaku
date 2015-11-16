79733507e81e184d8182a2f1bfddeb3c0c2db294
Simon Cruanes <simon.cruanes.2007@m4x.org>
2015-11-16 13:56:43 +0100

    `flatten-equation` now also eliminates pattern matching

--- src/core/transformations/nunFlattenEquation.ml
+++ src/core/transformations/nunFlattenEquation.ml
@@ -6,7 +6,7 @@
 module ID = NunID
 module Var = NunVar
 module Stmt = NunStatement
-module TI = NunTermInner
+module TI = NunTermMono
 module TyI = NunTypeMono
 module Subst = Var.Subst
 module Env = NunEnv
@@ -26,8 +26,9 @@ let errorf_ fmt = NunUtils.exn_ksprintf fmt ~f:(fun msg -> error_ msg)
 let section = NunUtils.Section.make "flatten_eqn"
 
 module Make(T : NunTermInner.S) = struct
-  module U = TI.Util(T)
-  module P = TI.Print(T)
+  module U = NunTermInner.Util(T)
+  module P = NunTermInner.Print(T)
+  module TMono = NunTermMono.Make(T)
   module TyMono = NunTypeMono.Make(T)
 
   type term = T.t
@@ -88,7 +89,7 @@ module Make(T : NunTermInner.S) = struct
      Returns a context updated with the necessary substitutions and constraints *)
   let rec mk_eq ~ctx ~to_ t =
     let t = U.deref ~subst:ctx.subst t in
-    match T.repr t with
+    match TMono.repr t with
     | TI.Var v ->
         assert (not (Subst.mem ~subst:ctx.subst v));
         if blocked_var_ ~ctx v
@@ -97,18 +98,17 @@ module Make(T : NunTermInner.S) = struct
     | TI.Const id -> mk_eq_constr ~ctx ~to_ id []
     | TI.App (f, l) ->
         (* we only deal with [constructor l] *)
-        begin match T.repr f with
+        begin match TMono.repr f with
         | TI.Const id -> mk_eq_constr ~ctx ~to_ id l
         | _ -> errorf_ "expected first-order pattern, got %a" P.print t
         end
     | TI.AppBuiltin (_,_)
     | TI.Let (_,_,_)
     | TI.Match (_,_) ->
-        NunUtils.not_implemented "flatten equation: non inductive pattern"
+        NunUtils.not_implemented "flatten equation: non inductive pattern on LHS"
     | TI.TyBuiltin _
     | TI.TyArrow (_,_) -> errorf_ "expected pattern, got type %a" P.print t
     | TI.Bind (_,_,_) -> errorf_ "expected pattern, got %a" P.print t
-    | TI.TyMeta _ -> assert false
 
   and mk_eq_constr ~ctx ~to_ id l =
     (* find the declaration/definition of [id] *)
@@ -131,6 +131,62 @@ module Make(T : NunTermInner.S) = struct
     | _ ->
         errorf_ "%a is not a (co)inductive constructor" ID.print_name id
 
+  let mk_select_ c i t = U.app_builtin (`DataSelect(c,i)) [t]
+  let mk_test_ c t = U.app_builtin (`DataTest c) [t]
+  let mk_ite_ a b c = U.app_builtin `Ite [a;b;c]
+
+  (* apply substitution [ctx.subst] in [t], and also replace pattern matching
+     with [`DataSelect] and [`DataTest] *)
+  let rec elim_match_ ~subst t = match TMono.repr t with
+    | TI.Var v ->
+        CCOpt.get t (Subst.find ~subst v)
+    | TI.Const _ -> t
+    | TI.App (f,l) -> U.app (elim_match_ ~subst f) (elim_match_l_ ~subst l)
+    | TI.AppBuiltin (b,l) -> U.app_builtin b (elim_match_l_ ~subst l)
+    | TI.Bind ((`Forall | `Exists | `Fun) as b,v,t) ->
+        let v' = Var.fresh_copy v in
+        let subst = Subst.add ~subst v (U.var v') in
+        let t' = elim_match_ ~subst t in
+        U.mk_bind b v' t'
+    | TI.Let (v,t,u) ->
+        let t' = elim_match_ ~subst t in
+        let v' = Var.fresh_copy v in
+        let subst = Subst.add ~subst v (U.var v') in
+        U.let_ v' t' (elim_match_ ~subst u)
+    | TI.TyBuiltin _ -> t
+    | TI.TyArrow (a,b) ->
+        U.ty_arrow (elim_match_ ~subst a)(elim_match_ ~subst b)
+    | TI.Match (t,l) ->
+        (* change t into t';
+            then a decision tree is built where
+              each case   [c,vars,rhs] is changed into:
+              "if is-c t' then rhs[vars_i := select-c-i t'] else ..."
+        *)
+        let t' = elim_match_ ~subst t in
+        (* remove first binding to make it the default case *)
+        let c1, (vars1,rhs1) = ID.Map.choose l in
+        let subst1 = CCList.Idx.foldi
+          (fun subst i vi -> Subst.add ~subst vi (mk_select_ c1 i t'))
+          subst vars1
+        in
+        let default_case = elim_match_ ~subst:subst1 rhs1 in
+        (* series of ite with selectors on the other cases *)
+        let l = ID.Map.remove c1 l in
+        ID.Map.fold
+          (fun c (vars,rhs) acc ->
+            let subst' = CCList.Idx.foldi
+              (fun subst i vi -> Subst.add ~subst vi (mk_select_ c i t'))
+              subst vars
+            in
+            let rhs' = elim_match_ ~subst:subst' rhs in
+            mk_ite_ (mk_test_ c t') rhs' acc
+          )
+          l
+          default_case
+
+  and elim_match_l_ ~subst l =
+    List.map (elim_match_ ~subst) l
+
   (* @return (v, ctx') where [ctx'] is a superset of [ctx] and [v] is a
      variable that does not occur in [ctx.vars]
      @param ty the type of the argument
@@ -140,7 +196,7 @@ module Make(T : NunTermInner.S) = struct
   : ctx:ctx -> ty:term -> term -> ctx * var
   = fun ~ctx ~ty t ->
     NunUtils.debugf ~section 3 "@[<2>flatten pattern `@[%a@]`@]" (fun k->k P.print t);
-    match T.repr t with
+    match TMono.repr t with
     | TI.Var v ->
         if blocked_var_ ~ctx v
         then
@@ -157,22 +213,21 @@ module Make(T : NunTermInner.S) = struct
     | TI.AppBuiltin _ ->
         (* replace [t] with a fresh var [v] and add [v = t] as a guard
           to the RHS. *)
-        (* TODO: use [head ty] as a name? *)
-        let v = Var.make ~ty ~name:"v" in
+        let name = try ID.name (U.head_sym ty) with _ -> "v" in
+        let v = Var.make ~ty ~name in
         (* add constraints for [v = t] *)
         let ctx = mk_eq ~ctx ~to_:(U.var v) t in
         ctx, v
     | TI.TyBuiltin _
     | TI.TyArrow (_,_) -> errorf_ "expected pattern, got %a" P.print t
-    | TI.Bind (`TyForall, _, _)
-    | TI.TyMeta _ -> assert false
-
 
   let flatten_eqn ~defined ~env e =
     let Stmt.Eqn_nested (_vars,args,rhs,side) = e in
     (* type of the defined term *)
     let ty_head = find_ty_ ~env defined.Stmt.defined_head in
     let ty_args = ty_args_ ty_head in
+    NunUtils.debugf ~section 5 "type arguments: %a, term arguments: %a"
+      (fun k->k(CCFormat.list P.print) ty_args (CCFormat.list P.print) args);
     assert (List.length ty_args = List.length args);
     (* map every argument to a variable, accumulating constraints and
        bindings along the way *)
@@ -185,22 +240,25 @@ module Make(T : NunTermInner.S) = struct
         ctx', v
       ) (empty_ctx ~env) (List.combine args ty_args)
     in
-    let rhs' = U.eval ~subst:ctx.subst rhs in
-    let side = List.map (U.eval ~subst:ctx.subst) side in
+    let rhs' = elim_match_ ~subst:ctx.subst rhs in
+    let side = elim_match_l_ ~subst:ctx.subst side in
     (* add constraints to [side] *)
     let side' = List.map
       (fun constr -> match constr with
         | EqTerm (t1,t2) ->
             (* t1=t2 => rhs *)
-            let t1 = U.eval ~subst:ctx.subst t1 in
-            let t2 = U.eval ~subst:ctx.subst t2 in
+            let t1 = elim_match_ ~subst:ctx.subst t1 in
+            let t2 = elim_match_ ~subst:ctx.subst t2 in
             U.eq t1 t2
         | Test (t, id) ->
-            let t = U.eval ~subst:ctx.subst t in
+            let t = elim_match_ ~subst:ctx.subst t in
             mk_data_test_ ~id t
       ) ctx.c_set
     in
     Stmt.Eqn_linear (vars, rhs', List.rev_append side' side)
+
+  let elim_match_top_ t = elim_match_ ~subst:Subst.empty t
+  let id_ x = x
 
   (* translate one single statement *)
   let tr_statement ~env st =
@@ -218,9 +276,11 @@ module Make(T : NunTermInner.S) = struct
         let env = Env.rec_funs ~env l' in
         env, Stmt.axiom_rec ~info l'
     | Stmt.Axiom (Stmt.Axiom_spec l) ->
+        let l = Stmt.map_spec_defs ~term:elim_match_top_ ~ty:id_ l in
         let env = Env.spec_funs ~env l in
         env, Stmt.axiom_spec ~info l
     | Stmt.Axiom (Stmt.Axiom_std l) ->
+        let l = List.map elim_match_top_ l in
         env, Stmt.axiom ~info l
     | Stmt.Decl (id,k,ty) ->
         let env = Env.declare ~kind:k ~env id ty in
@@ -228,7 +288,9 @@ module Make(T : NunTermInner.S) = struct
     | Stmt.TyDef (k,l) ->
         let env = Env.def_data ~env ~kind:k l in
         env, Stmt.mk_ty_def ~info k l
-    | Stmt.Goal g -> env, Stmt.goal ~info g
+    | Stmt.Goal g ->
+        let g = elim_match_top_ g in
+        env, Stmt.goal ~info g
 
   let tr_problem pb =
     let env = Env.create() in
