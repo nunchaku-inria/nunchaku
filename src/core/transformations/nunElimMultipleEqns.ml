520daaf025167351fff990634cf5377b7491d9f8
Simon Cruanes <simon.cruanes.2007@m4x.org>
2015-11-23 18:47:34 +0100

    wip: elimination of multiple equations

--- src/core/transformations/nunElimMultipleEqns.ml
+++ src/core/transformations/nunElimMultipleEqns.ml
@@ -7,6 +7,8 @@ module ID = NunID
 module TI = NunTermInner
 module Var = NunVar
 module Stmt = NunStatement
+module Subst = Var.Subst
+module Env = NunEnv
 
 type id = NunID.t
 type 'a var = 'a Var.t
@@ -15,9 +17,11 @@ type ('a,'b) inv1 = <ty:'a; eqn:'b>
 type 'a inv2 = <ty:'a; eqn:[`Single]>
 
 module Make(T : TI.S) = struct
+  module U = TI.Util(T)
   module P = TI.Print(T)
 
   type term = T.t
+  type subst = (term,term) Subst.t
 
   exception Error of string
 
@@ -32,56 +36,130 @@ module Make(T : TI.S) = struct
   let error_ msg = raise (Error msg)
   let errorf_ msg = NunUtils.exn_ksprintf msg ~f:error_
 
-  (* decision tree for pattern matching *)
-  type dtree =
-    | Yield of term  (* rhs *)
-    | Undefined
-    | Decide of int * dtree ID.Map.t (* switch on the given sub-term *)
+  (* new [`Undefined] instance of [t] *)
+  let mk_undefined t =
+    let id = ID.make ~name:"u" in
+    U.app_builtin (`Undefined id) [t]
 
-  (* list of terms [l] leads to [rhs]; add this to [dtree].
-    @param i offset in the list of terms *)
-  let rec dtree_add_ i l rhs dt =
-    match l with
-    | [] ->
-        begin match dt with
-        | Undefined -> Yield rhs
-        | Yield rhs' ->
-            errorf_
-              "@[<hv2>ambiguous matching yielding@ `@[%a@]`@ or `@[%a@]`@]"
-              P.print rhs P.print rhs'
-        | Decide _ -> error_ "incompatible lengths of arguments"
+  let mk_ite a b c = U.app_builtin `Ite [a;b;c]
+  let mk_and l = U.app_builtin `And l
+
+  type local_state = {
+    root: term; (* term being pattern matched on (for undefined) *)
+    env: term Env.t;
+    subst: subst; (* for renaming variables, mostly *)
+  }
+
+  type pattern =
+    | P_var of term var
+    | P_term of term  (* should be a pattern, we will see *)
+    | P_any (* no pattern, always succeeds *)
+
+  (* returns a pair [l1, l2] where [l1] contains RHS terms with no side
+     conditions, and [l2] contains RHS terms with their condition *)
+  let extract_sides l =
+    let rec aux noside_acc side_acc l = match l with
+      | [] -> noside_acc, side_acc
+      | (rhs, []) :: l' -> aux (rhs :: noside_acc) side_acc l'
+      | (rhs, ((_::_) as sides) :: l' ->
+          aux noside_acc ((mk_and sides, Yield rhs) :: side_acc) l'
+    in
+    aux [] [] l
+
+  (* TODO: change a [equations] into a match tree *)
+  (* transform flat equations into a match tree. *)
+  let rec equations_to_match_tree ~local_state vars l =
+    match vars, l with
+    | _, [] -> mk_undefined local_state.root (* undefined case *)
+    | [], [([], rhs, [])] ->
+        (* simple case: no side conditions, one RHS *)
+        U.eval ~subst:local_state.subst rhs
+    | [], l ->
+        (* add side conditions *)
+        let noside, side = extract_sides l in
+        begin match noside with
+          | t1::t2::_, _ ->
+              errorf_ "@[ambiguity: terms `@[%a@]`@ and `@[%a@]`@ have no side conditions@]"
+                P.print t1 P.print t2
+          | [], l ->
+              (* try conditions one by one, fail otherwise *)
+              let default = mk_undefined local_state.root in
+              List.fold_left
+                (fun acc (cond,rhs) ->
+                  let rhs = U.eval ~subst:local_state.subst rhs in
+                  mk_ite cond rhs acc)
+                default l
+          | [rhs], [] ->
+              U.eval ~subst:local_state.subst rhs
+          | [t], _::_ ->
+              errorf_
+                "@[ambiguity: term `@[%a@]`@ has no side conditions,@ but other terms do@]"
+                P.print t
         end
-    | t :: l' ->
-        begin match T.repr t, dt with
-        | TI.Var v, _ -> assert false
-        end
+    | v::vars', l ->
+        (* match [v] against the first pattern of each element of [l] *)
+        let trees = List.map
+          (fun (pat::pats, rhs, side) ->
+            pat, equations_to_match_tree ~local_state vars' [pats,rhs,side])
+          l
+        in
+        Case (v, trees)
 
   let uniq_eqns
   : type a b.
+    env:(term, term, (a,b) inv1) env ->
+    id:id ->
     (term, term, (a,b) inv1) NunStatement.equations ->
     (term, term, a inv2) NunStatement.equations
-  = function
+  = fun ~env ~id e -> match e with
   | Stmt.Eqn_single (vars,rhs) -> Stmt.Eqn_single (vars, rhs)
   | Stmt.Eqn_linear l ->
-      assert false 
+      assert false  (* TODO: already done? should do a Ite on side conditions? *)
+  | Stmt.Eqn_nested l ->
+      (* create fresh vars *)
+      let vars = match l with
+        | [] -> assert false
+        | (_, args, rhs, _) :: _ ->
+            List.mapi
+              (fun i a ->
+                let ty = U.ty_exn ~sigma:(Env.find_ty ~env) a in
+                Var.make ~ty (spf "v_%d" i))
+              args
+      and cases = List.map (fun (_,args,rhs,side) -> args, rhs, side) l in
+      let local_state = {
+        root=U.app (U.const id) (List.map U.var vars); (* defined term *)
+        env;
+        subst=Subst.empty;
+      }
+      equations_to_match_tree ~local_state vars cases
 
-  let uniq_eqn_st st =
+  let uniq_eqn_st env st =
     let info = Stmt.info st in
     match Stmt.view st with
     | Stmt.Axiom (Stmt.Axiom_rec l) ->
-        let l' = List.map 
-          (fun def -> {def with Stmt.rec_eqns=uniq_eqns def.Stmt.rec_eqns})
+        let l' = List.map
+          (fun def ->
+            let id = def.Stmt.rec_defined.Stmt.defined_head in
+            let rec_eqns = uniq_eqns ~id ~env def.Stmt.rec_eqns in
+            {def with Stmt.rec_eqns; }
           l
         in
         Stmt.axiom_rec ~info l'
-    | Stmt.Axiom (Stmt.Axiom_spec l) -> Stmt.axiom_spec ~info l
-    | Stmt.Axiom (Stmt.Axiom_std l) -> Stmt.axiom ~info l
-    | Stmt.Decl (id,kind,ty) -> Stmt.mk_decl ~info id kind ty
-    | Stmt.TyDef (k,ty) -> Stmt.mk_ty_def ~info k ty
+    | Stmt.Axiom (Stmt.Axiom_spec l) -> env, Stmt.axiom_spec ~info l
+    | Stmt.Axiom (Stmt.Axiom_std l) -> env, Stmt.axiom ~info l
+    | Stmt.Decl (id,kind,ty) ->
+        let env = Env.declare ~env id ty in
+        env, Stmt.mk_decl ~info id kind ty
+    | Stmt.TyDef (k,ty) ->
+        (* declare (co)data, so it can be used in encoding *)
+        let env = Env.def_data ~env ~kind:k ty in
+        env, Stmt.mk_ty_def ~info k ty
     | Stmt.Goal g -> Stmt.goal ~info g
 
-  let uniq_eqns_pb pb = NunProblem.map_statements ~f:uniq_eqn_st pb
-
+  let uniq_eqns_pb pb =
+    let _, pb' = NunProblem.fold_map_statements pb
+      ~f:uniq_eqn_st ~x:(Env.create()) in
+    pb'
 
   let pipe ~decode ~print =
     let on_encoded = if print
