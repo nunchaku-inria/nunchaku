465ae5a691ae21fcc1951f0b7690998da404a323
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-03-29 15:53:45 +0200

    implement eta-reduction

--- src/core/Reduce.mli
+++ src/core/Reduce.mli
@@ -6,16 +6,20 @@
 (* TODO: delta-reduction (expand definitions of Const) *)
 
 module Make(T : TermInner.S) : sig
+  type subst = (T.t,T.t) Var.Subst.t
+
   val whnf : T.t -> T.t
   (** Weak Head Normal Form *)
 
   val snf : T.t -> T.t
   (** Strong Normal Form (reduce under functions) *)
 
-  type subst = (T.t,T.t) Var.Subst.t
-
   val app_whnf : ?subst:subst -> T.t -> T.t list -> T.t
   (** [app_whnf f l] applies [f] to [l], then computes the weak head normal form *)
+
+  val eta_reduce : T.t -> T.t
+(** Eta-reduction at the root of the term.
+    This replaces [λx. f x] with [f], if [f] does not contain [x] *)
 
   module Full : sig
     val whnf : ?subst:subst-> T.t -> T.t list -> (T.t * T.t list * subst)
