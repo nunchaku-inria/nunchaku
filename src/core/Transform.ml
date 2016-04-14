b49522bd69e3819846b6cbd147d83066f8f896fd
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-04-14 15:47:44 +0200

    change deeply the `Transform` module, now with forks, close, etc.

--- src/core/Transform.ml
+++ src/core/Transform.ml
@@ -5,9 +5,6 @@
 
 type 'a printer = 'a CCFormat.printer
 
-type 'a lazy_list = unit -> [`Nil | `Cons of 'a * 'a lazy_list]
-(** A lazy list of values of type ['a] *)
-
 (** {2 Single Transformation} *)
 
 (** Transformation of ['a] to ['b]. The transformation make choices by
@@ -18,7 +15,7 @@ type ('a, 'b, 'c, 'd) t = Ex : ('a, 'b, 'c, 'd, 'st) inner -> ('a, 'b, 'c, 'd) t
 (** Transformation with explicit hidden state *)
 and ('a, 'b, 'c, 'd, 'st) inner = {
   name : string; (** name for the transformation, used for debug, CLI options, etc. *)
-  encode : 'a -> ('b * 'st) lazy_list;
+  encode : 'a -> ('b * 'st);
   decode : 'st -> 'c -> 'd;
   mutable on_input : ('a -> unit) list;
   mutable on_encoded : ('b -> unit) list;
@@ -41,15 +38,11 @@ let make ?print ?(on_input=[])
     print_state=print;
   }
 
-let make1 ?print ?on_input ?on_encoded ?on_decoded ~name ~encode ~decode =
-  let encode x = CCKList.return (encode x) in
-  make ?print ?on_input ?on_encoded ?on_decoded ~name ~encode ~decode
-
 let backward ~name f =
   let decode () x = f x in
-  make1 ~name ~encode:(fun x->x,()) ~decode ()
+  make ~name ~encode:(fun x->x,()) ~decode ()
 
-let nop () = make1 ~name:"nop" ~encode:(fun x->x,()) ~decode:(fun () y->y) ()
+let nop () = make ~name:"nop" ~encode:(fun x->x,()) ~decode:(fun () y->y) ()
 
 let on_input (Ex tr) ~f = tr.on_input <- f :: tr.on_input
 let on_encoded (Ex tr) ~f = tr.on_encoded <- f :: tr.on_encoded
@@ -60,23 +53,59 @@ let on_encoded (Ex tr) ~f = tr.on_encoded <- f :: tr.on_encoded
 
 module Pipe = struct
   type ('a, 'b, 'c, 'd) t =
-    | Id : ('a, 'a, 'c, 'c) t  (** no transformation *)
-    | Comp : ('a, 'b, 'd, 'e) transformation * ('b, 'b2, 'c, 'd) t -> ('a, 'b2, 'c, 'e) t
+    | Id : ('a, 'a, 'c, 'c) t (** no transformation *)
+    | Fail : ('a, 'b, 'c, 'd) t (** yields empty list *)
+    | Flatten :
+        ('a, 'b list, 'c, 'd) t ->
+        ('a, 'b, 'c, 'd) t
+    | Close :
+        ('b1 -> ('c1 -> 'd) -> 'b2 * ('c2 -> 'd)) *
+        ('a, 'b1, 'c1, 'd) t ->
+        ('a, 'b2, 'c2, 'd) t
+    | Comp :
+        ('a, 'b, 'e, 'f) transformation *
+        ('b, 'c, 'd, 'e) t ->
+        ('a, 'c, 'd, 'f) t
+    | Fork :
+        ('a, 'b, 'c, 'd) t *
+        ('a, 'b, 'c, 'd) t ->
+        ('a, 'b, 'c, 'd) t
 
   let id = Id
+  let fail = Fail
+  let flatten t = Flatten t
+  let close ~f p = Close (f,p)
   let compose a p = Comp (a, p)
+  let fork
+    : type a b c d.
+      (a, b, c, d) t ->
+      (a, b, c, d) t ->
+      (a, b, c, d) t
+    = fun a b -> match a, b with
+      | Fail, o
+      | o, Fail -> o
+      | Id, Id -> Id
+      | _ -> Fork (a,b)
+  let rec fork_l = function
+    | [] -> Fail
+    | [a] -> a
+    | a :: b -> fork a (fork_l b)
   let (@@@) = compose
+  let fork_comp l kont =
+    fork_l (List.map (fun t -> t @@@ kont) l)
 
   let fpf = Format.fprintf
   let print out t =
-    let first = ref true in
     let rec pp : type a b c d. (a,b,c,d) t printer
     = fun out t -> match t with
-    | Id -> ()
+    | Id -> fpf out "id"
+    | Fail -> fpf out "fail"
+    | Flatten t -> fpf out "flatten {@[%a@]}" pp t
+    | Close (_, pipe) -> fpf out "close {@[%a@]}" pp pipe
     | Comp (Ex tr, t') ->
-        if !first then first:=false else fpf out " ≡≡@ ";
-        fpf out "%s" tr.name;
-        pp out t'
+        fpf out "@[%s ≡≡@ %a@]" tr.name pp t'
+    | Fork (a,b) ->
+        fpf out "fork {@[<v> @[%a@]@,| @[%a@]@]}" pp a pp b
     in
     fpf out "@[%a@]" pp t
 end
@@ -85,50 +114,33 @@ end
 let callbacks_ l x = List.iter (fun f -> f x) l
 
 let rec run
-  : type a b c d. pipe:(a,b,c,d) Pipe.t -> a -> (b * (c -> d)) lazy_list
-  = fun ~pipe x -> match pipe with
-  | Pipe.Id -> CCKList.return (x, fun x->x)
-  | Pipe.Comp (Ex trans, pipe') ->
-      let open CCKList in
-      callbacks_ trans.on_input x;
-      trans.encode x
-      >>= fun (y, st) ->
-      callbacks_ trans.on_encoded y;
-      run ~pipe:pipe' y
-      >|= fun (y', conv_back) ->
-      let conv_back' x =
-        let res = trans.decode st (conv_back x) in
-        callbacks_ trans.on_decoded res;
-        res
-      in
-      y', conv_back'
-
-(** {2 Pipe with a function at the end} *)
-module ClosedPipe = struct
-  type ('a, 'c, 'd, 'res) t =
-    | ClosedEx : ('a, 'b, 'c, 'd, 'res) inner -> ('a, 'c, 'd, 'res) t
-  (** A machine that consumes ['a] using a pipeline, and calls some function
-      to obtain a result ['res] and something that can be converted back
-      to ['d] using the pipeline again *)
-
-  and ('a, 'b, 'c, 'd, 'res) inner = {
-    pipe: ('a, 'b, 'c, 'd) Pipe.t;
-    call: ('b -> 'res lazy_list);
-  }
-
-  let make ~pipe ~f = ClosedEx {pipe; call=f }
-
-  let make1 ~pipe ~f =
-    make ~pipe ~f:(fun x -> CCKList.return (f x))
-
-  let print out (ClosedEx t) = Pipe.print out t.pipe
-end
-
-let run_closed ~cpipe:(ClosedPipe.ClosedEx cpipe) a =
-  run ~pipe:cpipe.ClosedPipe.pipe a
-  |> CCKList.flat_map
-    (fun (b, conv_back) ->
-      cpipe.ClosedPipe.call b
-      |> CCKList.map (fun res -> res, conv_back))
-
-
+  : type a b c d. pipe:(a,b,c,d) Pipe.t -> a -> (b * (c -> d)) Lazy_list.t
+  = fun ~pipe x ->
+    let open Lazy_list.Infix in
+    match pipe with
+      | Pipe.Id -> Lazy_list.return (x, fun x->x)
+      | Pipe.Fail -> Lazy_list.empty
+      | Pipe.Flatten pipe' ->
+        run ~pipe:pipe' x
+        >>= fun (l, ret) ->
+        (* yield each element of [l] separately *)
+        Lazy_list.of_list l
+        >|= fun y -> y, ret
+      | Pipe.Close (f, pipe) ->
+        let l = run ~pipe x in
+        l >|= fun (y, back) ->
+        f y back
+      | Pipe.Comp (Ex trans, pipe') ->
+        callbacks_ trans.on_input x;
+        let y, st = trans.encode x in
+        callbacks_ trans.on_encoded y;
+        run ~pipe:pipe' y
+        >|= fun (y', conv_back) ->
+        let conv_back' x =
+          let res = trans.decode st (conv_back x) in
+          callbacks_ trans.on_decoded res;
+          res
+        in
+        y', conv_back'
+      | Pipe.Fork (a, b) ->
+        Lazy_list.append (run ~pipe:a x) (run ~pipe:b x)
