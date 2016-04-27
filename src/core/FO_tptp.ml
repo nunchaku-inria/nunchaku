32df59afde82d1c3f7560e33f0e43d669266b349
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-04-27 17:42:19 +0200

    add `FO_tptp`, representation for basic untyped TPTP formulas/terms

---
+++ src/core/FO_tptp.ml
@@ -0,0 +1,137 @@
+
+(* This file is free software, part of nunchaku. See file "license" for more details. *)
+
+(** {1 TPTP representation} *)
+
+type ty = Unitype
+type var = ty Var.t
+
+type term =
+  | App of ID.t * term list
+  | Var of var
+
+type form =
+  | True
+  | False
+  | And of form list
+  | Or of form list
+  | Not of form
+  | Imply of form * form
+  | Equiv of form * form
+  | Atom of term
+  | Eq of term * term
+  | Neq of term * term
+  | Forall of var * form
+  | Exists of var * form
+
+type role =
+  | R_axiom
+  | R_conjecture
+
+type statement = {
+  st_name: string;
+  st_role: role;
+  st_form: form;
+}
+
+let app id l = App (id,l)
+let const id = app id []
+let var v = Var v
+let true_ = True
+let false_ = False
+let and_ = function
+  | [] -> True
+  | [x] -> x
+  | l -> And l
+let or_ = function
+  | [] -> False
+  | [x] -> x
+  | l -> Or l
+let imply a b = Imply (a,b)
+let equiv a b = Equiv (a,b)
+let atom t = Atom t
+let eq a b = Eq (a,b)
+let neq a b = Neq (a,b)
+let not_ = function
+  | Neq (a,b) -> eq a b
+  | Eq (a,b) -> neq a b
+  | Not f -> f
+  | f -> Not f
+let forall v f = Forall (v,f)
+let exists v f = Exists (v,f)
+let forall_l = List.fold_right forall
+let exists_l = List.fold_right exists
+
+module E = ID.Erase
+
+let erase = E.create_state ()
+
+let fpf = Format.fprintf
+let pp_list ?(sep=", ") pp = CCFormat.list ~start:"" ~stop:"" ~sep pp
+
+let print_role_tptp out = function
+  | R_axiom -> CCFormat.string out "axiom"
+  | R_conjecture -> CCFormat.string out "conjecture"
+
+let pp_var out v =
+  (* build deterministic name for this variable *)
+  let n = Var.name v |> String.capitalize in
+  let n = Printf.sprintf "%s_%d" n (Var.id v |> ID.id) in
+  CCFormat.string out n
+
+(* map ID to a name, unambiguously *)
+let name_of_id_ id = E.to_name erase id |> String.uncapitalize
+
+let rec print_term_tptp out = function
+  | Var v -> pp_var out v
+  | App (id,[]) -> CCFormat.string out (name_of_id_ id)
+  | App (id,l) ->
+    fpf out "%s(@[<hv>%a@])" (name_of_id_ id) (pp_list print_term_tptp) l
+
+type prec =
+  | P_not
+  | P_and
+  | P_or
+  | P_bind
+
+let right_assoc_ = function
+  | P_and
+  | P_or -> false
+  | P_not
+  | P_bind -> true
+
+(* put () if needed *)
+let wrap p1 p2 out fmt =
+  if p1>p2 || (p1 = p2 && (not (right_assoc_ p1)))
+  then (
+    CCFormat.string out "(";
+    Format.kfprintf (fun _ -> CCFormat.string out ")") out fmt
+  )
+  else Format.kfprintf (fun _ -> ()) out fmt
+
+let print_form_tptp out f =
+  let rec aux p out = function
+    | True -> CCFormat.string out "$true"
+    | False -> CCFormat.string out "$false"
+    | Atom t -> print_term_tptp out t
+    | And [] | Or [] -> assert false
+    | And l -> wrap P_and p out "@[<hv>%a@]" (pp_list ~sep:" & " (aux P_and)) l
+    | Or l -> wrap P_or p out "@[<hv>%a@]" (pp_list ~sep:" | " (aux P_or)) l
+    | Not f -> wrap P_not p out "@[~@ @[%a@]@]" (aux P_not) f
+    | Imply (a,b) ->
+      wrap P_or p out "@[@[%a@] =>@ @[%a@]@]" (aux P_or) a (aux P_or) b
+    | Equiv (a,b) ->
+      wrap P_or p out "@[@[%a@] <=>@ @[%a@]@]" (aux P_or) a (aux P_or) b
+    | Eq (a,b) ->
+      fpf out "@[@[%a@] =@ @[%a@]@]" print_term_tptp a print_term_tptp b
+    | Neq (a,b) ->
+      fpf out "@[@[%a@] !=@ @[%a@]@]" print_term_tptp a print_term_tptp b
+    | Forall (v,f) -> wrap P_bind p out "@[![%a]:@ @[%a@]@]" pp_var v (aux P_bind) f
+    | Exists (v,f) -> wrap P_bind p out "@[?[%a]:@ @[%a@]@]" pp_var v (aux P_bind) f
+  in
+  aux P_bind out f
+
+let print_statement_tptp out st =
+  fpf out "@[<2>fof(@[%s, %a,@ @[%a@]@]).@]"
+    st.st_name print_role_tptp st.st_role print_form_tptp st.st_form
+
---
+++ src/core/FO_tptp.mli
@@ -0,0 +1,67 @@
+
+(* This file is free software, part of nunchaku. See file "license" for more details. *)
+
+(** {1 TPTP representation}
+
+    Representation of terms and formulas for FOF (untyped TPTP) *)
+
+type ty = Unitype
+type var = ty Var.t
+
+type term =
+  | App of ID.t * term list
+  | Var of var
+
+type form =
+  | True
+  | False
+  | And of form list
+  | Or of form list
+  | Not of form
+  | Imply of form * form
+  | Equiv of form * form
+  | Atom of term
+  | Eq of term * term
+  | Neq of term * term
+  | Forall of var * form
+  | Exists of var * form
+
+type role =
+  | R_axiom
+  | R_conjecture
+
+type statement = {
+  st_name: string;
+  st_role: role;
+  st_form: form;
+}
+
+val const : ID.t -> term
+val app : ID.t -> term list -> term
+val var : var -> term
+
+val true_ : form
+val false_ : form
+val and_ : form list -> form
+val or_ : form list -> form
+val imply : form -> form -> form
+val equiv : form -> form -> form
+val atom : term -> form
+val not_ : form -> form
+val eq : term -> term -> form
+val neq : term -> term -> form
+val forall : var -> form -> form
+val forall_l : var list -> form -> form
+val exists : var -> form -> form
+val exists_l : var list -> form -> form
+
+val erase : ID.Erase.state
+(** Used to map IDs to names during printing *)
+
+val print_role_tptp : role CCFormat.printer
+val print_term_tptp : term CCFormat.printer
+val print_form_tptp : form CCFormat.printer
+val print_statement_tptp : statement CCFormat.printer
+
+
+
