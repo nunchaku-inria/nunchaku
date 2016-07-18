fbd80bc713b46b4cadd54400b532bbd3b64356c1
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-07-18 17:37:10 +0200

    implement ElimCopy as a datatype

--- src/transformations/ElimCopy.ml
+++ src/transformations/ElimCopy.ml
@@ -14,88 +14,97 @@ module P = T.P
 let name = "elim_copy"
 
 type term = T.t
+type ty = T.t
 
-(* id function of type [ty -> ty] *)
-let id_ ty =
-  let v = Var.make ~name:"x" ~ty in
-  U.fun_ v (U.var v)
+type decode_state = (term, ty) Env.t
 
-let as_copy_ ~env id =
-  let info = Env.find_exn ~env id in
-  match Env.def info with
-  | Env.Copy_abstract c -> `Abs c
-  | Env.Copy_concrete c -> `Conc c
-  | Env.Copy_ty c -> `Ty c
-  | _ -> `Not_copy
-
-(* recursively rewrite the term, doing:
-  - rewrite terms by removing abstract/concretization functions
-  - change type to its definition, including in variables and other types
-    (should be just a constant since we are after monomorphization)
-  - TODO insert `asserting pred` if the predicate is not None
-  *)
-let rec rewrite_term ~env subst t = match T.repr t with
-  | TI.Var v ->
-      begin match Var.Subst.find ~subst v with
-      | None -> t
-      | Some v' -> U.var v'
-      end
-  | TI.Const id ->
-      begin match as_copy_ ~env id with
-      | `Not_copy -> t
-      | `Abs c
-      | `Conc c -> id_ c.Stmt.copy_of (* identity *)
-      | `Ty c -> c.Stmt.copy_of
-      end
-  | TI.App (f, l) ->
-      let l = List.map (rewrite_term ~env subst) l in
-      begin match T.repr f with
-      | TI.Const id ->
-          begin match as_copy_ ~env id, l with
-          | _, [] -> assert false
-          | (`Abs _ | `Conc _), f :: l -> U.app f l (* erase *)
-          | `Ty _, _ -> assert false
-          | `Not_copy, _ -> rewrite_rec_ ~env subst t
-          end
-      | _ -> rewrite_rec_ ~env subst t
-      end
-  | _ -> rewrite_rec_ ~env subst t
-
-and rewrite_rec_ ~env subst t =
-  U.map subst t ~bind:(bind_var ~env) ~f:(rewrite_term ~env)
-
-and bind_var ~env subst v =
-  (* maybe the type of [v] contains occurrences of copy types *)
-  let ty = Var.ty v in
-  let ty = rewrite_term ~env subst ty in
-  let v' = Var.make ~name:(Var.name v) ~ty in
-  Var.Subst.add ~subst v v', v'
+(* encode the copy type as a datatype *)
+let copy_as_data ~info (c:_ Stmt.copy): _ Stmt.t list =
+  (* the datatype itself, whose cstor is [c.copy_abstract] *)
+  let cstor =
+    { Stmt.
+      cstor_name = c.Stmt.copy_abstract;
+      cstor_args = [c.Stmt.copy_of];
+      cstor_type = U.ty_arrow c.Stmt.copy_of c.Stmt.copy_to
+    }
+  in
+  let data_ =
+    { Stmt.
+      ty_id = c.Stmt.copy_id;
+      ty_vars = [];
+      ty_type = U.ty_type;
+      ty_cstors = ID.Map.singleton c.Stmt.copy_abstract cstor
+    }
+  in
+  let decl_data = Stmt.data ~info [data_] in
+  (* the definition of [c.copy_concrete], as pattern matching:
+     [forall x:concrete. concr (abs x) = x] *)
+  let decl_concr =
+    let x = Var.make ~ty:c.Stmt.copy_of ~name:"x" in
+    let lhs = U.app_const c.Stmt.copy_abstract [U.var x] in
+    let rhs = U.var x in
+    let def =
+      { Stmt.
+        rec_defined = Stmt.mk_defined c.Stmt.copy_concrete c.Stmt.copy_concrete_ty;
+        rec_vars=[];
+        rec_eqns=Stmt.Eqn_nested [[x], [lhs], rhs, []];
+      }
+    in
+    Stmt.axiom_rec ~info:Stmt.info_default [def]
+  in
+  (* return all new decls *)
+  [decl_data; decl_concr]
 
 let elim pb =
   let env = Problem.env pb in
-  Problem.flat_map_statements pb
-  ~f:(fun st ->
-    match Stmt.view st with
-    | Stmt.Copy _ -> [] (* remove *)
-    |_ ->
-        let f = rewrite_term ~env in
-        [Stmt.map_bind Var.Subst.empty st ~bind:(bind_var ~env) ~term:f ~ty:f ])
+  let pb' =
+    Problem.flat_map_statements pb
+      ~f:(fun stmt ->
+        let info = Stmt.info stmt in
+        match Stmt.view stmt with
+          | Stmt.Copy c ->
+            begin match c.Stmt.copy_pred with
+              | None -> copy_as_data ~info c
+              | Some _ -> assert false (* TODO *)
+            end
+          | _ -> [stmt]
+      )
+  in
+  pb', env
+
+let decode_model (st:decode_state) m : _ Model.t =
+  let env = st in
+  Model.filter m
+    ~funs:(fun (t,_,_,_) -> match T.repr t with
+      | TI.Const id ->
+        begin match Env.find ~env id with
+          | Some {Env.def=Env.Copy_concrete c; _} ->
+            (* drop `concrete` functions *)
+            begin match c.Stmt.copy_pred with
+              | None -> false
+              | Some _ -> assert false (* TODO: set of rewrite rules on constants *)
+            end
+          | _ -> true
+        end
+      | _ -> true
+    )
 
 let pipe ~print ~check =
   let on_encoded =
     Utils.singleton_if print () ~f:(fun () ->
       let module Ppb = Problem.Print(P)(P) in
-      Format.printf "@[<v2>@{<Yellow>after elimination of copy types@}:@ %a@]@." Ppb.print)
+      Format.printf "@[<v2>@{<Yellow>after %s@}:@ %a@]@." name Ppb.print)
     @
     Utils.singleton_if check () ~f:(fun () ->
       let module C = TypeCheck.Make(T) in
       C.empty () |> C.check_problem)
   in
+  let decode st = Problem.Res.map_m ~f:(decode_model st) in
   Transform.make
     ~name
     ~input_spec:Transform.Features.(of_list [Ty, Mono; Copy, Present])
-    ~map_spec:Transform.Features.(update Copy Absent)
+    ~map_spec:Transform.Features.(update_l [Copy, Absent; Data, Present; Fun, Eqn_nested])
     ~on_encoded
-    ~encode:(fun pb -> elim pb, ())
-    ~decode:(fun () x -> x)
+    ~encode:elim
+    ~decode
     ()
