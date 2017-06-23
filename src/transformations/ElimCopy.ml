f0d2656f1881d162dd52bcc99f48cf902107cefd
Simon Cruanes <simon.cruanes.2007@m4x.org>
2017-06-23 15:13:47 +0200

    bugfix in ElimCopy: for subset types, only put upper bound on cardinality

--- src/transformations/ElimCopy.ml
+++ src/transformations/ElimCopy.ml
@@ -86,14 +86,17 @@ let copy_as_data ~info (c:(_,_) Stmt.copy): (_,_) Stmt.t list =
 
 let approx_threshold_ = 30 (* FUDGE *)
 
-(* should we do an approximation of [c.Stmt.copy_of]? *)
-let should_be_incomplete card_concrete = match card_concrete with
+(* do we have an exact cardinal for [c.Stmt.copy_of], that is
+   also small enough? *)
+let has_small_card card_concrete = match card_concrete with
   | Cardinality.Infinite
-  | Cardinality.Unknown -> true
+  | Cardinality.Unknown -> None
   | Cardinality.Exact n
   | Cardinality.QuasiFiniteGEQ n ->
     (* if [n >= threshold] we approximate *)
-    Cardinality.Z.(compare (of_int approx_threshold_) n <= 0)
+    if Cardinality.Z.(compare n (of_int approx_threshold_) <= 0)
+    then Some (Cardinality.Z.to_int n |> CCOpt.get_exn)
+    else None
 
 let attrs_of_ty state (ty:ty): Stmt.decl_attr list =
   (if AT.is_abstract state.env ty
@@ -109,7 +112,8 @@ let copy_subset_as_uninterpreted_ty state ~info ~(pred:term) c : (_, _) Stmt.t l
   let card_concrete =
     AT.cardinality_ty ~cache:state.at_cache state.env c.Stmt.copy_of
   in
-  let incomplete = should_be_incomplete card_concrete in
+  let card_abstract_upper_bound = has_small_card card_concrete in
+  let incomplete = card_abstract_upper_bound = None in
   let id_c = c.Stmt.copy_id in
   let ty_c = U.ty_const id_c in
   ID.Tbl.add state.copy_as_uninterpreted id_c ();
@@ -122,7 +126,13 @@ let copy_subset_as_uninterpreted_ty state ~info ~(pred:term) c : (_, _) Stmt.t l
   (* declare the new (uninterpreted) type and functions *)
   let decl_c =
     let new_attr =
-      if incomplete then Stmt.Attr_incomplete else Stmt.Attr_card_hint card_concrete
+      if incomplete then Stmt.Attr_incomplete
+      else match card_abstract_upper_bound with
+        | None -> assert false
+        | Some n ->
+          (* must be an exact cardinal, but for the subtype it's only
+             an upper bound *)
+          Stmt.Attr_card_max n
     in
     let old_attrs = attrs_of_ty state c.Stmt.copy_of in
     let attrs = new_attr :: old_attrs in
